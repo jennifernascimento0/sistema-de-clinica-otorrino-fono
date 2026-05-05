13e961bc63ba73a2e6e86b4e9f249a50de43838b
mahsalves <mhsa4@discente.ifpe.edu.br>
2026-05-05 20:27:47 -0300

    adicionei campo de mensagens no base.html e template de registro de consulta. tentei mudar os urls pra conseguir visualiazar as págs normalmente, mas n consegui

--- clinica/clinica/urls.py
+++ clinica/clinica/urls.py
@@ -15,8 +15,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from . import views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+     # pág inicial!!!
+    path('', views.home, name='home'),
+    # quando o usuário digitar /pacientes/, ele chama a view que a gente criou
+    path('pacientes/', views.paciente_list, name='paciente_list'),
 ]
