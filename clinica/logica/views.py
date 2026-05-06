32432a8ffcba5f7035298ee7f2099ef5e06928e7
JenniferNascimento0 <jenniferdnascimento02@gmail.com>
2026-05-06 13:31:30 -0300

    criar pacientes

--- clinica/logica/views.py
+++ clinica/logica/views.py
@@ -1,11 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from .models import Paciente, Consulta, Profissional
-
+#listar parcientes
 def paciente_list(request):
     # aqui faz a busca de todos os pacientes cadastrados no banco de dados
     pacientes = Paciente.objects.all()
     # e aqui envia os dados para o template paciente_list.html
     return render(request, 'logica/paciente_list.html', {'pacientes': pacientes})
+#criar pacientes
+def criar_paciente(request):
+    if request.method == 'POST':
+        Paciente.objects.create( 
+            nome=request.POST .get('nome'),
+            cpf=request.POST.get('cpf'),
+            telefone=request.POST.get('telefone'),
+            email=request.POST.get('email'))
+        return redirect('paciente_list')
+    return render(request,'pacientes/criar.html')
 
 def home(request):
     return render(request, 'logica/home.html')
